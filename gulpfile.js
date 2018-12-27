d87c684cfcc70f2ddf99f1819830364fb66628da
qing.liu <qing.liu@its.cn>
2018-12-27 11:27:15 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,16 @@ gulp.task('compile', function() {
       cascade: false
     }))
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib'));
+    .pipe(gulp.dest('E:\\all-project\\VAP\\01-Dev\\branches\\vap-VUE\\theme'));
+    // .pipe(gulp.dest('./lib'));
 });
 
 gulp.task('copyfont', function() {
+    console.log('copyfont');
   return gulp.src('./src/fonts/**')
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib/fonts'));
+    .pipe(gulp.dest('E:\\all-project\\VAP\\01-Dev\\branches\\vap-VUE\\theme\\fonts'));
+    // .pipe(gulp.dest('./lib/fonts'));
 });
 
 gulp.task('build', ['compile', 'copyfont']);
@@ -29,22 +32,24 @@ gulp.task('build', ['compile', 'copyfont']);
 // 创建文件修改监听任务
 gulp.task('auto',function(){
     // 源码有改动就进行压缩以及热刷新
-    gulp.watch('src/*.scss',['compile'])
-    gulp.watch('lib/*.css',['reload'])
-    gulp.watch('./index.html',['reload'])
+    gulp.watch('src/*/*.scss',['build'])
+    gulp.watch('lib/*/*.css',['reload'])
+    gulp.watch('index.html',['reload'])
 })
 
 // 创建热加载任务
 gulp.task('reload',function(){
     gulp.src('lib/*')
         .pipe(connect.reload())
-    console.log('html change')
+    gulp.src('index.html')
+        .pipe(connect.reload());
 })
 
 // gulp服务器
 gulp.task('server',function(){
     connect.server({
         root:'test',
+        port: 8899,
         livereload:true
     })
 });
